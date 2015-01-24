8da14bc86d942731e7c4d4e9a390e4aa39bc567c
Yohannes Tamru <yohannist@gmail.com>
2015-01-24 15:40:35 +0300

    User agent and other configurations text moved to the app.config file with access given through SpiderInfo class

---
+++ Spidy/Utility/SpiderInfo.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Configuration;
+
+namespace Spider.Utility
+{
+    public static class SpiderInfo
+    {
+        public static string Useragent
+        {
+            get { return ConfigurationManager.AppSettings["userAgent"]; }
+        }
+
+        public static string UseragentEmailAddress
+        {
+            get { return ConfigurationManager.AppSettings["userAgentEmailAddress"]; }
+        }
+
+        public static string UseragentDocumentation
+        {
+            get { return ConfigurationManager.AppSettings["userAgentDocumentation"]; }
+        }
+
+    }
+}
